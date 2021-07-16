914192725041e8f75340c9231f9cb7f1d40e44d1
Kunal Yelne <kunal17058@gmail.com>
2021-07-16 20:18:07 +0530

    Adds Array Example

--- src/main/kotlin/collections/Examples.kt
+++ src/main/kotlin/collections/Examples.kt
@@ -5,4 +5,18 @@ fun main() {
     a.sort()
     a.add(43)
     println(a)
+    val set = mutableSetOf<Int>(1,2,1,4,5,8,3)
+    println(set)
+    var iterator = a.iterator()
+    while(iterator.hasNext()) {
+        print("${iterator.next()} ")
+    }
+    println()
+    for (i in 0..10) print(i)
+    println()
+    val ar = Array(10) {"kyo"}
+    for( i in ar) print(i)
+    println()
+    val arr = IntArray(10)
+    for( i in arr) print(i)
 }
